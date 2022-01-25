887a9260ad47bde55e9cb7f798b2ab59e6cc241d
rtviii <rtkushner@gmail.com>
2022-01-24 23:49:29 -0800

    testnet endpoints

--- memio.sh
+++ memio.sh
@@ -9,8 +9,8 @@ echo $pid
 echo > logs/ens5.txt
 echo > logs/nvme0n1.txt
 echo > logs/pidstat.txt
-
 elapsed=0
+
 while kill -0 $pid 2> /dev/null; do
     echo "Elapsed $elapsed s."
     elapsed=$(($elapsed + 1))
@@ -19,3 +19,7 @@ while kill -0 $pid 2> /dev/null; do
     vmstat  -w -S M   >>  logs/vmstat_$TAG.txt
     sar -n DEV  1 1 | awk 'NR==5 {print}' >> logs/ens5_$TAG.txt
 done
+
+#-⋯⋯⋅⋱⋰⋆⋅⋅⋄⋅⋅∶⋅⋅⋄▫▪▭┈┅✕⋅⋅⋄⋅⋅✕∶⋅⋅⋄⋱⋰⋯⋯⋯⋯⋅⋱⋰⋆⋅⋅⋄⋅⋅∶⋅⋅⋄▫▪▭┈[ Network ]┅✕⋅⋅⋄⋅⋅✕∶⋅⋅⋄⋱⋰⋯⋯⋯⋅⋱⋰⋆⋅⋅⋄⋅⋅∶⋅⋅⋄▫▪▭┈┅✕⋅⋅⋄⋅⋅✕∶⋅⋅⋄⋱⋰⋯⋯⋯
+sudo iftop -t  -s 1 | awk 'NR==10||NR==11||NR==12{print}'
+# 
--- script.js
+++ script.js
@@ -1,55 +1,63 @@
 import http from 'k6/http';
 import { Trend, Rate, Counter, Gauge } from 'k6/metrics';
-
 //@ts-check
 export const fail_counter = new Counter('FailedRequests')
 
 //-⋯⋯⋅⋱⋰⋆⋅⋅⋄⋅⋅∶⋅⋅⋄▫▪▭┈┅✕⋅⋅⋄⋅⋅✕∶⋅⋅⋄⋱⋰⋯⋯⋯⋯⋅⋱⋰⋆⋅⋅⋄⋅⋅∶⋅⋅⋄▫▪▭┈┅✕⋅⋅⋄⋅⋅✕∶⋅⋅⋄⋱⋰⋯⋯⋯⋅⋱⋰⋆⋅⋅⋄⋅⋅∶⋅⋅⋄▫▪▭┈┅✕⋅⋅⋄⋅⋅✕∶⋅⋅⋄⋱⋰⋯⋯⋯
-const endpoints = [
-  '/cosmos/base/tendermint/v1beta1/blocks/latest',         // 36k
-  '/cosmos/base/tendermint/v1beta1/validatorsets/latest'   // 28K
-  // '/blocks/latest', 
-  // '/wasm/contracts/terra1cgg6yef7qcdm070qftghfulaxmllgmvk77nc7t/store?query_msg={"prices":{}}',
-  // '/wasm/contracts/terra1e25zllgag7j9xsun3me4stnye2pcg66234je3u/store?query_msg={"config":{}}',
-  // '/wasm/contracts/terra1cf9q9lq7tdfju95sdw78y9e34a6qrq3rrc6dre/store?query_msg={"state":{}}',
-  // '/wasm/contracts/terra1c0afrdc5253tkp5wt7rxhuj42xwyf2lcre0s7c/store?query_msg={"pool":{}}',
-  // '/wasm/contracts/terra1jxazgm67et0ce260kvrpfv50acuushpjsz2y0p/store?query_msg={"pool":{}}',
-  // '/wasm/contracts/terra1tndcaqxkpc5ce9qee5ggqf430mr2z3pefe5wj6/store?query_msg={"pool":{}}',
-  // '/wasm/contracts/terra1xu8utj38xuw6mjwck4n97enmavlv852zkcvhgp/store?query_msg={"polls":{}}',
-]
 
 export const options = {
-  // ext: {
-  //   loadimpact: {
-  //     apm: [
-  //       {
-  //         provider: 'prometheus',
-  //         remoteWriteURL: 'https://prometheus-prod-10-prod-us-central-0.grafana.net/api/prom/push',
-  //         credentials: {
-  //           username: '308504',
-  //           password: 'eyJrIjoiZDM1MWRlMzkwMGRkMGVlMWE5NTAyNzc2M2Q5MmRhNmI0ZTc2NjU3YSIsIm4iOiJ0ZXJyYWs2IiwiaWQiOjU5MTQ5N30=',
-  //         },
-  //         // optional parameters
-  //         // metrics: ['http_req_sending', 'my_rate', 'my_gauge'], // ...
-  //         includeDefaultMetrics: true,
-  //         includeTestRunId: false,
-  //       },
-  //     ],
-  //   },
-  // },
   vus       : 200,
   duration  : "10s",
-  // thresholds: {
-    // http_req_failed: [{ threshold: 'count<1', abortOnFail: true, delayAbortEval: '2s'}],
-    // FailedRequests : [{ threshold: 'count<1', abortOnFail: true, delayAbortEval: '2s'}],
-  // },
 };
 
 export default function () {
-  // Randomized query
-  // http.get( encodeURI(`http://127.0.0.1:1317${endpoints[Math.floor(Math.random() * 10)]}`)) 
-http.get(encodeURI('http://127.0.0.1:1317/cosmos/base/tendermint/v1beta1/blocks/latest'))
-  // resp.status !== 200 ?    FailedReqs.add(1) : ( ()=>{} )();
-
-  // sleep(5)
+  http.get(encodeURI('http://127.0.0.1:1317/cosmos/base/tendermint/v1beta1/blocks/latest'))
 }
+
+const columbus_endpoints = [
+  '/cosmos/base/tendermint/v1beta1/blocks/latest',         // 36k
+  '/cosmos/base/tendermint/v1beta1/validatorsets/latest'   // 28K
+
+  // '/blocks/latest', 
+
+  // Anchor Price Oracle
+  // '/wasm/contracts/terra1cgg6yef7qcdm070qftghfulaxmllgmvk77nc7t/store?query_msg={"prices":{}}',
+
+  // Anchor Liquidation Queue
+  // '/wasm/contracts/terra1e25zllgag7j9xsun3me4stnye2pcg66234je3u/store?query_msg={"config":{}}',
+
+  // Kujira UST-KUJI Staking
+  // '/wasm/contracts/terra1cf9q9lq7tdfju95sdw78y9e34a6qrq3rrc6dre/store?query_msg={"state":{}}',
+
+  // bEth
+  // '/wasm/contracts/terra1c0afrdc5253tkp5wt7rxhuj42xwyf2lcre0s7c/store?query_msg={"pool":{}}',
+
+  // bLuna
+  // '/wasm/contracts/terra1jxazgm67et0ce260kvrpfv50acuushpjsz2y0p/store?query_msg={"pool":{}}',
+  // '/wasm/contracts/terra1seddp6u43xys0q85lpce9j6xwje7x7zqsf3fud/store?query_msg={"pool":{}}', 
+
+  // Terraswap LUNA-UST Pair
+  // '/wasm/contracts/terra1tndcaqxkpc5ce9qee5ggqf430mr2z3pefe5wj6/store?query_msg={"pool":{}}',
+
+  // Pylon Governance
+  // '/wasm/contracts/terra1xu8utj38xuw6mjwck4n97enmavlv852zkcvhgp/store?query_msg={"polls":{}}',
+]
+
+const bombay_endpoints = [
+  '/cosmos/base/tendermint/v1beta1/blocks/latest',         // 36k
+  '/cosmos/base/tendermint/v1beta1/validatorsets/latest',   // 28K
+  '/blocks/latest', 
+  // Anchor Price Oracle
+  '/wasm/contracts/terra1p4gg3p2ue6qy2qfuxtrmgv2ec3f4jmgqtazum8/store?query_msg={"prices":{}}',
+  // Anchor Liquidation Queue
+  '/wasm/contracts/terra18j0wd0f62afcugw2rx5y8e6j5qjxd7d6qsc87r/store?query_msg={"config":{}}',
+  // ANC token
+  '/wasm/contracts/terra1747mad58h0w4y589y3sk84r5efqdev9q4r02pc/store',
+  // bEth
+  '/wasm/contracts/terra19mkj9nec6e3y5754tlnuz4vem7lzh4n0lc2s3l/store?query_msg={"pool":{}}',
+  // bLuna
+  '/wasm/contracts/terra1seddp6u43xys0q85lpce9j6xwje7x7zqsf3fud/store?query_msg={"pool":{}}', 
+  // stLuna
+  '/wasm/contracts/terra1e42d7l5z5u53n7g990ry24tltdphs9vugap8cd/store?query_msg={"pool":{}}', 
+
+]
